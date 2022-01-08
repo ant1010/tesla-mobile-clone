a7f9d5db64c63dabaef08e84172c2c08052550bd
ant1010 <arreola.antony@gmail.com>
2022-01-07 18:29:44 -0700

    car item component

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground} from 'react-native';
+import CarItem from './components/CarItem';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    
+      <CarItem name = {"Model X"} tagLine = {"Starting at 99,500"} image = {require("./assets/images/ModelX.jpeg")}/>
+      <CarItem name = {"Model 3"} tagLine = {"Order Online for "} taglineCTA = {"Touchless Delivery"}image = {require("./assets/images/Model3.jpeg")}/>
+      <CarItem name = {"Model S"} tagLine = {"Starting at 69,420"} image = {require("./assets/images/ModelS.jpeg")}/>
+      <CarItem name = {"Model Y"} tagLine = {"Starting at 60,500"} image = {require("./assets/images/ModelY.jpeg")}/>
+
+
+   
+      
       <StatusBar style="auto" />
+      
     </View>
+    
   );
 }
 
@@ -17,4 +28,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+ 
 });
