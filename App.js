56258c23091628a99cf688a516837357f5c90ef0
ant1010 <arreola.antony@gmail.com>
2022-01-07 21:45:59 -0700

    scrollable car list and header

--- App.js
+++ App.js
@@ -1,18 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ImageBackground} from 'react-native';
-import CarItem from './components/CarItem';
+import React from 'react';
+import { StyleSheet, View} from 'react-native';
+import CarsList from './components/CarsList';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <View style={styles.container}>
     
-      <CarItem name = {"Model X"} tagLine = {"Starting at 99,500"} image = {require("./assets/images/ModelX.jpeg")}/>
-      <CarItem name = {"Model 3"} tagLine = {"Order Online for "} taglineCTA = {"Touchless Delivery"}image = {require("./assets/images/Model3.jpeg")}/>
-      <CarItem name = {"Model S"} tagLine = {"Starting at 69,420"} image = {require("./assets/images/ModelS.jpeg")}/>
-      <CarItem name = {"Model Y"} tagLine = {"Starting at 60,500"} image = {require("./assets/images/ModelY.jpeg")}/>
-
-
-   
+      <Header></Header>
+      
+      <CarsList/>
+    
       
       <StatusBar style="auto" />
       
